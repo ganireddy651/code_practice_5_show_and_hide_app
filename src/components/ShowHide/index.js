743b00d155171e5b941a49405ab4f0b1bbc6e914
root <ganireddy651>
2023-05-20 12:33:50 +0000

    functionality

--- src/components/ShowHide/index.js
+++ src/components/ShowHide/index.js
@@ -1,41 +1,41 @@
-// Write your code here
 import {Component} from 'react'
 import './index.css'
 
 class ShowHide extends Component {
-  state = {firstNameCard: false, lastNameCard: false}
+  state = {firstName: false, lastName: false}
 
   firstName = () => {
-    this.setState(prevState => {})
+    const {firstName} = this.state
+    this.setState(() => ({
+      firstName: !firstName,
+    }))
   }
 
-  lastName = () => {}
+  lastName = () => {
+    const {lastName} = this.state
+    this.setState(() => ({
+      lastName: !lastName,
+    }))
+  }
 
   render() {
-    const {firstNameCard, lastNameCard} = this.state
+    const {firstName, lastName} = this.state
+
     return (
       <div className="hide-and-show-bg-container">
         <h1 className="heading">Show/Hide</h1>
-        <div className="button-container">
-          <button onClick={this.firstName} className="button" type="button">
-            Show/Hide Firstname
-          </button>
-          <button onClick={this.lastName} className="button" type="button">
-            Show/Hide Lastname
-          </button>
-        </div>
-        <div className="card-container">
-          <div
-            onChange={this.firstName()}
-            className={`first-name-card ${firstNameCard}`}
-          >
-            <p>Joe</p>
+        <div className="hide-and-show-cards-container">
+          <div className="first-name-section">
+            <button onClick={this.firstName} type="button" className="button">
+              Show/Hide Firstname
+            </button>
+            {firstName ? <p className="first-name-card">Joe</p> : ''}
           </div>
-          <div
-            onChange={this.lastName()}
-            className={`last-name-card ${lastNameCard}`}
-          >
-            <p>Jonas</p>
+          <div className="last-name-section">
+            <button onClick={this.lastName} type="button" className="button">
+              Show/Hide Lastname
+            </button>
+            {lastName ? <p className="last-name-card">Jonas</p> : ''}
           </div>
         </div>
       </div>
